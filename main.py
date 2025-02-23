68c3d5acf9d452ae5899e08a3c2f513c7c13bfce
Masha <maryme887@yandex.ru>
2025-02-23 21:53:34 +0700

    чтото

---
+++ main.py
@@ -0,0 +1,42 @@
+import os
+import sys
+
+import pygame
+import requests
+
+server_address = 'https://static-maps.yandex.ru/v1?'
+api_key = 'f3a0fe3a-b07e-4840-a1da-06f18b2ddf13'
+cord_x, cord_y = input('Введите нужные координаты(две переменные через пробел)').split()
+ll_spn = 'll=37.530887,55.703118&spn=0.002,0.002'
+spn = input('Введите маштаб (одна десятичная дробь)')
+# Готовим запрос.
+
+paramas = {
+    'll': ','.join([cord_x, cord_y]),
+    'spn': ','.join([spn, spn]),
+    'api_key': api_key
+}
+response = requests.get(server_address, params=paramas)
+
+if not response:
+    print("Ошибка выполнения запроса:")
+    print("Http статус:", response.status_code, "(", response.reason, ")")
+    sys.exit(1)
+# Запишем полученное изображение в файл.
+map_file = "map.png"
+with open(map_file, "wb") as file:
+    file.write(response.content)
+
+# Инициализируем pygame
+pygame.init()
+screen = pygame.display.set_mode((600, 450))
+# Рисуем картинку, загружаемую из только что созданного файла.
+screen.blit(pygame.image.load(map_file), (0, 0))
+# Переключаем экран и ждем закрытия окна.
+pygame.display.flip()
+while pygame.event.wait().type != pygame.QUIT:
+    pass
+pygame.quit()
+
+# Удаляем за собой файл с изображением.
+os.remove(map_file)

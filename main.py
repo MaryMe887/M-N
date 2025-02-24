66a5fcc4edcc5310e99a8629be53d826270d0b39
anast <anastasia@borodulina.me>
2025-02-24 14:02:28 +0700

    2 задание

--- main.py
+++ main.py
@@ -1,42 +1,60 @@
 import os
 import sys
-
 import pygame
 import requests
 
-server_address = 'https://static-maps.yandex.ru/v1?'
+server_address = 'https://static-maps.yandex.ru/v1'
 api_key = 'f3a0fe3a-b07e-4840-a1da-06f18b2ddf13'
-cord_x, cord_y = input('Введите нужные координаты(две переменные через пробел)').split()
-ll_spn = 'll=37.530887,55.703118&spn=0.002,0.002'
-spn = input('Введите маштаб (одна десятичная дробь)')
-# Готовим запрос.
+min_spn, max_spn = 0.0005, 50
+cord_x, cord_y = input('Введите нужные координаты (две переменные через пробел): ').split()
+spn = float(input('Введите масштаб (одна десятичная дробь): '))
 
-paramas = {
-    'll': ','.join([cord_x, cord_y]),
-    'spn': ','.join([spn, spn]),
-    'apikey': api_key
-}
-response = requests.get(server_address, params=paramas)
 
-if not response:
-    print("Ошибка выполнения запроса:")
-    print("Http статус:", response.status_code, "(", response.reason, ")")
-    sys.exit(1)
-# Запишем полученное изображение в файл.
-map_file = "map.png"
-with open(map_file, "wb") as file:
-    file.write(response.content)
+def load_map():
+    """Готовим запрос"""
+    global spn
+    spn = max(min_spn, min(max_spn, spn))
+    print(f"Текущий масштаб: {spn}")
 
-# Инициализируем pygame
+    params = {
+        'll': f'{cord_x},{cord_y}',
+        'spn': f'{spn},{spn}',
+        'apikey': api_key
+    }
+    response = requests.get(server_address, params=params)
+
+    if not response:
+        print("Ошибка выполнения запроса:")
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+        sys.exit(1)
+
+    with open("map.png", "wb") as file:
+        file.write(response.content)
+
+# Инициализация pygame
 pygame.init()
 screen = pygame.display.set_mode((600, 450))
-# Рисуем картинку, загружаемую из только что созданного файла.
-screen.blit(pygame.image.load(map_file), (0, 0))
-# Переключаем экран и ждем закрытия окна.
-pygame.display.flip()
-while pygame.event.wait().type != pygame.QUIT:
-    pass
+pygame.display.set_caption("Карта")
+load_map()
+running = True
+while running:
+    map_image = pygame.image.load("map.png")
+    screen.blit(map_image, (0, 0))
+    pygame.display.update()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                spn /= 1.5
+                print(spn)
+                load_map()
+            elif event.key == pygame.K_DOWN:
+                spn *= 1.5
+                load_map()
+
 pygame.quit()
 
-# Удаляем за собой файл с изображением.
-os.remove(map_file)
+if os.path.exists("map.png"):
+    os.remove("map.png")
